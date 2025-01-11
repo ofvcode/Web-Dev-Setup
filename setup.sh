38550e19deb2229710aca5efaa18980cb07a08db
ofvcode <adminofv@proton.me>
2025-01-11 17:09:15 -0500

    Fixed the html to have better standardized html boilderplate

--- setup.sh
+++ setup.sh
@@ -8,12 +8,18 @@ touch scripts/index.js
 
 
 echo "<!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel='stylesheet' href='stylesheets/styles.css'>
 </head>
 <body>
-    <script src='scripts/index.js'></script>
+    
+
+
+<script src='scripts/index.js'></script>
 </body>
 </html>" > index.html
 
